9cdb41e20b52b817c15c0d73bf8e5f5b315c4716
Carlos Villavicencio <cvillavicencio@stackbuilders.com>
2020-04-21 14:40:39 -0500

    Add tests

--- token.php
+++ token.php
@@ -8,33 +8,37 @@ use Twilio\Jwt\Grants\VoiceGrant;
 $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
 $dotenv->load();
 
+function get_access_token($identity) {
+    $access_token = new AccessToken(
+        getenv('TWILIO_ACCOUNT_SID'),
+        getenv('API_KEY'),
+        getenv('API_SECRET'),
+        3600,
+        $identity
+    );
+    
+    // Create Voice grant
+    $voiceGrant = new VoiceGrant();
+    $voiceGrant->setOutgoingApplicationSid(getenv('TWILIO_TWIML_APP_SID'));
+    
+    // Optional: add to allow incoming calls
+    $voiceGrant->setIncomingAllow(true);
+    
+    // Add grant to token
+    $access_token->addGrant($voiceGrant);
+    
+    // render token to string
+    $token = $access_token->toJWT();
+
+    return json_encode(array(
+        'identity' => $identity,
+        'token' => $token,
+    ));
+}
+
 // choose a random username for the connecting user
 $identity = randomUsername();
 
-$access_token = new AccessToken(
-    getenv('TWILIO_ACCOUNT_SID'),
-    getenv('API_KEY'),
-    getenv('API_SECRET'),
-    3600,
-    $identity
-);
-
-// Create Voice grant
-$voiceGrant = new VoiceGrant();
-$voiceGrant->setOutgoingApplicationSid(getenv('TWILIO_TWIML_APP_SID'));
-
-// Optional: add to allow incoming calls
-$voiceGrant->setIncomingAllow(true);
-
-// Add grant to token
-$access_token->addGrant($voiceGrant);
-
-// render token to string
-$token = $access_token->toJWT();
-
 // return serialized token and the user's randomly generated ID
 header('Content-Type: application/json');
-echo json_encode(array(
-    'identity' => $identity,
-    'token' => $token,
-));
+echo get_access_token($identity);
--- voice.php
+++ voice.php
@@ -6,23 +6,27 @@ use Twilio\TwiML\VoiceResponse;
 $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
 $dotenv->load();
 
-$response = new VoiceResponse();
+function get_voice_response($to) {
+    $response = new VoiceResponse();
 
-// get the phone number from the page request parameters, if given
-if (isset($_REQUEST['To']) && strlen($_REQUEST['To']) > 0) {
-    $number = htmlspecialchars($_REQUEST['To']);
-    $dial = $response->dial('', ['callerId' => getenv('TWILIO_CALLER_ID')]);
-    
-    // wrap the phone number or client name in the appropriate TwiML verb
-    // by checking if the number given has only digits and format symbols
-    if (preg_match("/^[\d\+\-\(\) ]+$/", $number)) {
-        $dial->number($number);
+    if (!empty($to) && strlen($to) > 0) {
+        $number = htmlspecialchars($to);
+        $dial = $response->dial('', ['callerId' => getenv('TWILIO_CALLER_ID')]);
+        
+        // wrap the phone number or client name in the appropriate TwiML verb
+        // by checking if the number given has only digits and format symbols
+        if (preg_match("/^[\d\+\-\(\) ]+$/", $number)) {
+            $dial->number($number);
+        } else {
+            $dial->client($number);
+        }
     } else {
-        $dial->client($number);
+        $response->say("Thanks for calling!");
     }
-} else {
-    $response->say("Thanks for calling!");
+    return (string)$response;
 }
 
+
+// get the phone number from the page request parameters, if given
 header('Content-Type: text/xml');
-echo $response;
+echo get_voice_response($_REQUEST['To'] ?? null);
